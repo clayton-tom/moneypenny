bf3ed8133dc5ca0e1e3b746f920999baa9e346f5
Tom Clayton <tom.clayton@stfc.ac.uk>
2021-01-30 14:59:02 +0000

    Add unit tests for format_message_to_str in core_io
    This also included addition of the regex crate as a dependency, as this is used to regex assert the timestamp in one of the tests

--- src/mp_core.rs
+++ src/mp_core.rs
@@ -25,6 +25,31 @@ pub mod core_io {
     fn print_to_output(message: String) {
         println!("{}", message);
     }
+
+    #[cfg(test)]
+    mod core_io_tests {
+        use crate::mp_core::Message;
+        use crate::mp_core::core_io::*;
+        use regex::Regex;
+
+        #[test]
+        fn test_format_message_to_str() {
+            let message = Message {
+                body: String::from("This is a test string."),
+                output_time: false,
+                sender: String::from("Test module"),
+            };
+            let expected_output = String::from("[Test module] This is a test string.");
+            assert_eq!(expected_output, format_message_to_str(message));
+            let message_time = Message {
+                body: String::from("This is another test string."),
+                output_time: true,
+                sender: String::from("Test module"),
+            };
+            let re = Regex::new(r#"\[Test module\] [A-Za-z]+ [0-9]+ [0-9]+:[0-9]+:[0-9]+: This is another test string."#).unwrap();
+            assert!(re.is_match(&format_message_to_str(message_time)));
+        }
+    }
 }
 
 mod core_time {
